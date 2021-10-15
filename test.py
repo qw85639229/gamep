a8cedded800c09ddb84ab8603a04e273b8cb5fdb
qw85639229 <1127772053@qq.com>
2021-10-15 14:12:06 +0800

    add png

--- test.py
+++ test.py
@@ -4,22 +4,9 @@ import time
 import threading as th
 import os
 import time
-time.sleep(2)
+import numpy as np
 
-def print_GetForegroundWindow():
-    hwnd_active = win32gui.GetForegroundWindow()
-    print('hwnd_active hwnd:',hwnd_active)
-    print('hwnd_active text:',win32gui.GetWindowText(hwnd_active))
-    print('hwnd_active class:',win32gui.GetClassName(hwnd_active))
-
-name = 'AntYecai'
-hwnd = win32gui.FindWindow(None, name)
-win32gui.BringWindowToTop(hwnd)
-win32gui.SetForegroundWindow(hwnd)
-win32gui.ShowWindow(hwnd, win32con.SW_SHOW)
-# win32gui.SetForegroundWindow(win32gui.FindWindow("UnityWndClass","AntYecai"))
-# print_GetForegroundWindow()
-# win32gui.ShowWindow(win32gui.FindWindow("UnityWndClass","AntYecai"), win32con.SW_SHOW)
-# if hwnd == 0:
-#     os.startfile('D:\\daily data\\AntYecaibuluo\\AntYecai.exe')
-
+a = np.random.random(4)
+print(a)
+print(a.argsort())
+print(a)
